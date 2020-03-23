7337df5ab385cffcfe32d3391dfa9a08c1303cf4
elviramg <54225565+elviramg@users.noreply.github.com>
2020-03-23 14:57:38 -0600

    Avance del proyecto

--- ProyectoFinal_EDA.c
+++ ProyectoFinal_EDA.c
@@ -34,5 +34,59 @@ typedef struct NewImp {
 
 int main(){
 	setlocale(LC_CTYPE, "Spanish");
+	
+	 do
+        {
+            system("cls");
+            printf("\n\t\t\t SIMULADORA DE UNA IMPRESORA")
+            int opc;
+            printf("\t\t\t BIENVENIDO \n\t");
+            printf("\n\t\t\t¿Qué deseas hacer?");
+            printf("\n\t\t\t1. Agregar impresora");
+            printf("\n\t\t\t2. Eliminar impresora");
+            printf("\n\t\t\t3. Visualizar colas de impresión");
+            printf("\n\t\t\t4. Mandar a imprimir");
+            printf("\n\t\t\t5. Añadir usuario");
+            printf("\n\t\t\t6. Salir");
+ 
+            fflush(stdin);
+            scanf("%d", &opc);
+
+            switch(opc){
+            case 1:
+               //agregar funcion
+                system("pause");
+                break;
+            case 2:
+            	//agregar función
+                system("pause");
+                break;
+            case 3:
+                //agregar funcion
+                system("pause");
+                break;
+            case 4:
+				//agregar funcion
+                system("pause");
+                break;
+            case 5:
+                //agregar funcion
+                system("pause");
+                break;
+            case 6:
+                printf("¡Adios!\n");
+                return 0;
+                break;
+            default:
+                printf("Opción no válida\n");
+                }
+                
+        }
+        while(1);
+
+    return 0;
+    }
+    
+    //FUNCIONES
 }
 
